ce98e3a6bd03a8f44b26b3971a7411358e3e8a06
Lucas-Braz7x <lucasdutrabraz@gmail.com>
2021-08-19 19:09:33 -0300

    Verificando se não há nenhum id repetido

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -6,21 +6,35 @@ module.exports = {
     const pass = req.body.password;
 
     let roomId;
+    let isRoom = true;
 
-    for(let i=0; i<6;i++){
-      i == 0 ? roomId = Math.floor(Math.random() * 10).toString() : 
-      roomId += Math.floor(Math.random() * 10).toString();
+    while(isRoom){
+        /* Gerando chave aleatória */
+      for(let i=0; i<6;i++){
+        i == 0 ? roomId = Math.floor(Math.random() * 10).toString() : 
+        roomId += Math.floor(Math.random() * 10).toString();
+      }
+
+      /* Verificando se já existe essa chave */
+      const roomsIds = await db.all(`SELECT id FROM rooms`);
+
+      isRoom = roomsIds.some(roomExistId => roomExistId == roomId);
+      if(!isRoom){
+        /* Inserindo a salano banco */
+        await db.run(`
+          INSERT INTO rooms(
+            id, 
+            pass
+          )VALUES(
+            ${parseInt(roomId)},
+            ${pass}
+          )
+        `);
+      }
+
     }
 
-    await db.run(`
-      INSERT INTO rooms(
-        id, 
-        pass
-      )VALUES(
-        ${parseInt(roomId)},
-        ${pass}
-      )
-    `);
+    
     await db.close();
 
     res.redirect(`/room/${roomId}`);
